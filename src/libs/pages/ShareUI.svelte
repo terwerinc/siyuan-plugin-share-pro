071628311efc42545880cfe70d34d6be7888d5ff
terwer <youweics@163.com>
2025-07-21 10:45:02 +0800

    build(dependencies): update dependencies and adjust import statements
    - Update @types/node to ^22.16.5
    - Update fast-glob to ^3.3.3
    - Update jsdom to ^26.1.0
    - Update siyuan to ^1.1.1
    - Update typescript to ^5.8.3
    - Update vite to ^5.4.19
    - Update vite-plugin-dts to ^4.5.4
    - Update vite-plugin-node-polyfills to ^0.24.0- Update vite-plugin-static-copy to ^3.1.1
    - Update vite-tsconfig-paths to ^5.1.4
    - Update vitest to ^3.2.4
    
    - Adjust import statements in ShareUI.svelte to use bracket notation
    - Comment out unused CSS styles in ShareUI.svelte

--- src/libs/pages/ShareUI.svelte
+++ src/libs/pages/ShareUI.svelte
@@ -43,7 +43,7 @@
         await shareService.createShare(docId)
       } catch (e) {
         formData.shared = false
-        showMessage(pluginInstance.i18n.ui.shareSuccessError, 3000, "info")
+        showMessage(pluginInstance.i18n["ui"]["shareSuccessError"], 3000, "info")
       }
     } else {
       // 状态检测
@@ -51,15 +51,15 @@
       // 注意 undefined
       if (formData?.shareData?.shareStatus && formData.shareData.shareStatus !== "COMPLETED") {
         formData.lock = true
-        showMessage(pluginInstance.i18n.ui.msgIngError, 3000, "info")
+        showMessage(pluginInstance.i18n["ui"]["msgIngError"], 3000, "info")
         return
       }
       // 取消分享
       const ret = await shareService.deleteDoc(docId)
       if (ret.code === 0) {
-        showMessage(pluginInstance.i18n.topbar.cancelSuccess, 3000, "info")
+        showMessage(pluginInstance.i18n["topbar"]["cancelSuccess"], 3000, "info")
       } else {
-        showMessage(pluginInstance.i18n.topbar.cancelError + ret.msg, 7000, "error")
+        showMessage(pluginInstance.i18n["topbar"]["cancelError"] + ret.msg, 7000, "error")
       }
     }
   }
@@ -82,7 +82,7 @@
     //         .replace(/\[title]/g, formData.post.title)
     //         .replace(/\[url]/g, formData.shareLink)
     copy(copyText)
-    showMessage(pluginInstance.i18n.ui.copySuccess, 3000, "info")
+    showMessage(pluginInstance.i18n["ui"]["copySuccess"], 3000, "info")
   }
 
   const viewDoc = () => {
@@ -164,7 +164,7 @@
     {#if formData.shared && !formData.lock}
       <div class="share-content">
         <div class="setting-row">
-          <span class="setting-label">{pluginInstance.i18n.ui.copyTitle}</span>
+          <span class="setting-label">{pluginInstance.i18n["ui"]["copyTitle"]}</span>
           <div class="input-group">
             <input
               type="text"
@@ -172,9 +172,9 @@
               readonly
               class="share-link-input"
               on:click={viewDoc}
-              title={pluginInstance.i18n.ui.clickView}
+              title={pluginInstance.i18n["ui"]["clickView"]}
             />
-            <button on:click={copyWebLink}>{pluginInstance.i18n.ui.copyWebLink}</button>
+            <button on:click={copyWebLink}>{pluginInstance.i18n["ui"]["copyWebLink"]}</button>
           </div>
         </div>
       </div>
@@ -249,7 +249,7 @@
     width 100%
 
   .input-group input,
-  .input-group select
+  //.input-group select
     flex-grow 1
     height 28px
     padding 4px 8px
@@ -267,7 +267,7 @@
   /* 防止 flex 布局导致超出容器 */
 
   .input-group input:focus,
-  .input-group select:focus
+  //.input-group select:focus
     outline none
     border-color #0073e6
     box-shadow 0 0 4px rgba(0, 115, 230, 0.5)
@@ -303,38 +303,48 @@
     background-color #005bb5
     transform scale(1.05)
 
-  .info-text
-    font-size 12px
-    margin-top 8px
-    margin-bottom 8px
-    color #ff4d4f
+  /* 重新分享 */
+  .reshare-container
+    display flex
+    align-items center
+    gap 12px
+  .reshare-btn
+    display inline-flex
+    cursor pointer
+    transition background-color 0.3s, box-shadow 0.3s
+    font-size 16px
+    line-height 1
+    color #333
 
+//  .info-text
+//    font-size 12px
+//    margin-top 8px
+//    margin-bottom 8px
+//    color #ff4d4f
+//
   html[data-theme-mode="light"]
-    .divider
-      background-color #e3e3e3
+    //.divider
+    //  background-color #e3e3e3
 
     .input-group input,
-    .input-group select
+    //.input-group select
       border-color #cccccc
       background-color #f9f9f9
 
-    .share-link-input
-      border-color #cccccc
-      background-color #f0f0f0
-      color #aaaaaa
+    //.share-link-input
+    //  border-color #cccccc
+    //  background-color #f0f0f0
+    //  color #aaaaaa
 
-    button
-      background-color #0073e6
-
-    button:hover
-      background-color #005bb5
-
-    .info-text
-      color #ff4d4f
+    //button
+    //  background-color #0073e6
+    //
+    //button:hover
+    //  background-color #005bb5
 
   html[data-theme-mode="dark"]
-    .divider
-      background-color #333333
+    //.divider
+    //  background-color #333333
 
     .input-group input
       border-color #444444
@@ -350,18 +360,18 @@
       background-color #3a3a3a
       box-shadow 0 0 4px rgba(0, 115, 230, 0.5)
 
-    .input-group select
-      border-color #444444
-      color var(--b3-theme-on-background)
-      background-color #2c2c2c
-
-    .input-group select option
-      color var(--b3-theme-on-background)
-      background-color #444444
-
-      &:hover, &:focus
-        background-color #555555
-        color #ffffff
+//    .input-group select
+//      border-color #444444
+//      color var(--b3-theme-on-background)
+//      background-color #2c2c2c
+//
+//    .input-group select option
+//      color var(--b3-theme-on-background)
+//      background-color #444444
+//
+//      &:hover, &:focus
+//        background-color #555555
+//        color #ffffff
 
     .share-link-input
       border-color #444444
@@ -374,28 +384,6 @@
     button:hover
       background-color #005bb5
 
-    .info-text
-      color #ff7875
-
-/* 重新分享 */
-  .reshare-container
-    display flex
-    align-items center
-    gap 12px
-
-  .reshare-btn
-    display inline-flex
-    cursor pointer
-    transition background-color 0.3s, box-shadow 0.3s
-    font-size 16px
-    line-height 1
-    color #333
-
-  html[data-theme-mode="dark"] &
-    color #e0e3e8
-
-  ::deep .reshare-btn svg
-    width 16px
-    height 16px
-    fill currentColor
+    //.reshare-btn
+    // color #fff
 </style>
