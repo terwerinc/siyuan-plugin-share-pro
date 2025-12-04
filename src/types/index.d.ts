2a34a844e4d0b84a7b0588a8624b3f32fb4489c9
terwer <youweics@163.com>
2025-12-04 23:38:49 +0800

    feat(types): add initial type definitions for share and service API
    - Add unified export file for all type definitions
    - Define IServiceApiConfig interface for service API config options
    - Create BlacklistItem and ShareBlacklist interfaces for managing share blacklist
    - Add BlacklistConfig interface for blacklist settings
    - Define ShareHistoryItem and ShareHistory interfaces for share history records management
    - Include GNU General Public License header in all new type definition files

---
+++ src/types/index.d.ts
@@ -0,0 +1,15 @@
+/*
+ *            GNU GENERAL PUBLIC LICENSE
+ *               Version 3, 29 June 2007
+ *
+ *  Copyright (C) 2025 Terwer, Inc. <https://terwer.space/>
+ *  Everyone is permitted to copy and distribute verbatim copies
+ *  of this license document, but changing it is not allowed.
+ */
+
+/**
+ * 统一导出所有类型定义
+ */
+export * from "./service-api"
+export * from "./share-history"
+export * from "./share-blacklist"
