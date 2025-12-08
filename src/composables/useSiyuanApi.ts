f0d3524b9f14c19946c600af126add8bcd0ecaa0
terwer <youweics@163.com>
2025-12-08 11:04:18 +0800

    chore(license): update copyright years to 2025
    - Updated copyright year range from 2024 to 2025 in multiple files
    - Standardized GNU GENERAL PUBLIC LICENSE header to Version 3, June 2007
    - Changed older GPL v2 license headers to GPL v3
    - Added missing license headers in several Svelte component files
    - Ensured all license notices permit verbatim copying and distribution without modifications

--- src/composables/useSiyuanApi.ts
+++ src/composables/useSiyuanApi.ts
@@ -2,10 +2,9 @@
  *            GNU GENERAL PUBLIC LICENSE
  *               Version 3, 29 June 2007
  *
- *  Copyright (C) 2024 Terwer, Inc. <https://terwer.space/>
+ *  Copyright (C) 2024-2025 Terwer, Inc. <https://terwer.space/>
  *  Everyone is permitted to copy and distribute verbatim copies
  *  of this license document, but changing it is not allowed.
- *
  */
 
 import { simpleLogger } from "zhi-lib-base"
